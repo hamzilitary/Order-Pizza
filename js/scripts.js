87b2dedd634b8043f013b0b3b68fe88c00314178
Guest User <Guest@Epicodus-8C.local>
2018-01-26 10:50:29 -0800

    init and add object Pizza

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function Pizza(size, bread, topping) {
+  this.size = size;
+  this.bread = bread;
+  this.topping = topping;
+}
+var pizzaPoints = function(inputtedSize, inputtedBread, inputtedTopping) {
+  var points = 0;
+    
+}

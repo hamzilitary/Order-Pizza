bba9ad3425caa1b61164318c68182028dd7d134b
Guest User <Guest@Epicodus-8C.local>
2018-01-26 11:32:12 -0800

    add pizzaPoints variable and drop down menus

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,26 @@ function Pizza(size, bread, topping) {
 }
 var pizzaPoints = function(inputtedSize, inputtedBread, inputtedTopping) {
   var points = 0;
-    
+    if (inputtedSize === "Large") {
+      points = points + 2;
+}   else if (inputtedSize === "Medium") {
+      points = points + 1;
+}   else {
+      points = points;
+}
+    if (inputtedBread === "Hand Tossed"){
+      points = points + 2;
+}   else if (inputtedBread === "Stuffed Crust") {
+      points = points + 3;
+}   else {
+      points = points + 1;
+}
+    if (inputtedTopping === "Cheese") {
+      points = points + 1;
+}   else if (inputtedTopping === "Pepperoni") {
+      points = points + 2;
+}   else {
+      points = points + 3;
+}
+      return points;
 }
